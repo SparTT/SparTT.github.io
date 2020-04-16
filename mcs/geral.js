adca37c9ce64d4a5a4c51b86b35baa351b6eaf0a
Leonardo <53948181+SparTT@users.noreply.github.com>
2020-04-16 17:34:06 -0300

    Create geral.js

---
+++ mcs/geral.js
@@ -0,0 +1,9 @@
+setInterval(function() {
+  if( $(window).scrollTop() > 75) {
+    document.querySelector('header').classList.add('fixed-top')
+    document.querySelector('#carousel-mcs').style.marginTop = '7rem'
+  }else {
+    document.querySelector('header').classList.remove('fixed-top')
+    document.querySelector('#carousel-mcs').style.marginTop = 'unset'
+  }
+})
