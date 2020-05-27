86bc7a1438e0de5d61c18312a34f760204e3cd4b
Leonardo <53948181+SparTT@users.noreply.github.com>
2020-05-27 12:39:54 -0300

    Add files via upload

---
+++ mcs/componentes.php
@@ -0,0 +1,31 @@
+﻿<?php
+
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => "http://minhacasasolar.dallasolucoes.com.br:888/api/millenium_eco_comp41/Produtos.ListaIdFotos?$format=json",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 0,
+  CURLOPT_FOLLOWLOCATION => true,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "GET",
+  CURLOPT_POSTFIELDS => "",
+  CURLOPT_HTTPHEADER => array(
+    "Content-Type: application/json"
+  ),
+));
+
+$response = curl_exec($curl);
+
+curl_close($curl);
+
+	$frase  = $response;
+	$saudavel = array("http:\/\/minhacasasolar.dallasolucoes.com.br:888\/api\/millenium!","millenium","MILLENIUM","http:\/\/minhacasasolar.dallasolucoes.com.br:888");
+	$saboroso   = array("server/","ERP","ERP","server/");
+	
+	$novafrase = str_replace($saudavel, $saboroso, $frase);
+
+
+  echo $novafrase;
